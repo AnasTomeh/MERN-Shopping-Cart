93b6c4519a4dc0cec41f75b3660568ba90577cc6
AnasTomeh <eng.anas.tomeh@gmail.com>
2022-03-26 23:37:33 +0300

    server

---
+++ server/models/productModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const productSchema = require('../Schema/productSchema')
+
+
+const Product = mongoose.model("product", productSchema)
+
+module.exports = Product
