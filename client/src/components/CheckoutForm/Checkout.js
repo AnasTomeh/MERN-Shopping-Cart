3c42e1c17d5a994acee607213e4cc45aaa2a832a
AnasTomeh <eng.anas.tomeh@gmail.com>
2022-03-26 16:31:26 +0300

    animation-part-2

--- client/src/components/CheckoutForm/Checkout.js
+++ client/src/components/CheckoutForm/Checkout.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import "../../css/CheckoutForm/Checkout.css"
 import Input from "../Input/input"
+import Zoom from 'react-reveal/Zoom'
 
 function Checkout(props) {
     return (
+
         <div> {props.showForm && <div className='checkout-form'>
             <span className='close-icon' onClick={() => props.setShowForm(false)}> &times; </span>
-            <form onSubmit={props.submitOreder}>
-                <Input
-                    label="Name"
-                    type="text"
-                    onChange={props.handleChange}
-                    name="name"
-                />
-                <Input
-                    label="Email"
-                    type="email"
-                    onChange={props.handleChange}
-                    name="email"
-                />
+            <Zoom left>
+                <form onSubmit={props.submitOreder}>
+                    <Input
+                        label="Name"
+                        type="text"
+                        onChange={props.handleChange}
+                        name="name"
+                    />
+                    <Input
+                        label="Email"
+                        type="email"
+                        onChange={props.handleChange}
+                        name="email"
+                    />
 
-                <div>
-                    <button type='submit'>Checkout</button>
-                </div>
-            </form>
+                    <div>
+                        <button type='submit'>Checkout</button>
+                    </div>
+                </form>
+            </Zoom>
         </div>}
         </div>
+
+
     )
 }
 export default Checkout;
--- client/src/components/Filtter/Filter.js
+++ client/src/components/Filtter/Filter.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import '../../css/Filtter/Filter.css'
+import Filp from "react-reveal/Flip"
 
 function Filter(props) {
   return (
-    <div className="Filter-wrapper">
-      <h2 className="Filter-title">Filter</h2>
-      <div className='num-of-products'>Number of Products {props.productsNumber}</div>
-      <div className='filter-by-size'>
-        <span>Filter</span>
-        <select className='filter-select' onChange={props.handelFilterBySize} value={props.size}>
-          <option value="ALL">ALL</option>
-          <option value="XS">XS</option>
-          <option value="S">S</option>
-          <option value="M">M</option>
-          <option value="L">L</option>
-          <option value="XL">XL</option>
-          <option value="XXL">XXL</option>
-        </select>
-      </div>
-      <div className='filter-by-size'>
-        <span>Order</span>
-        <select className='filter-select' value={props.sort} onChange={props.handelFilterByOrder}>
-          <option value="Latest">Latest</option>
-          <option value="Lowest">Lower</option>
-          <option value="Highest">Highest</option>
+    <Filp left>
+      <div className="Filter-wrapper">
+        <h2 className="Filter-title">Filter</h2>
+        <div className='num-of-products'>Number of Products {props.productsNumber}</div>
+        <div className='filter-by-size'>
+          <span>Filter</span>
+          <select className='filter-select' onChange={props.handelFilterBySize} value={props.size}>
+            <option value="ALL">ALL</option>
+            <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+          </select>
+        </div>
+        <div className='filter-by-size'>
+          <span>Order</span>
+          <select className='filter-select' value={props.sort} onChange={props.handelFilterByOrder}>
+            <option value="Latest">Latest</option>
+            <option value="Lowest">Lower</option>
+            <option value="Highest">Highest</option>
 
-        </select>
+          </select>
+        </div>
       </div>
-    </div>
+    </Filp>
   )
 }
 
