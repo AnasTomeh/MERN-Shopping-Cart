9e8e9c820cf2f2a2ded31d80ad14442653239fbc
AnasTomeh <eng.anas.tomeh@gmail.com>
2022-03-28 23:34:10 +0300

    add filter

--- client/src/components/Filtter/Filter.js
+++ client/src/components/Filtter/Filter.js
@@ -1,37 +1,48 @@
 import React from 'react'
 import '../../css/Filtter/Filter.css'
 import Filp from "react-reveal/Flip"
+import { connect } from "react-redux"
+import { filteredSize, filteredSort } from "../../store/actions/products"
 
 function Filter(props) {
   return (
     <Filp left>
-      <div className="Filter-wrapper">
-        <h2 className="Filter-title">Filter</h2>
-        <div className='num-of-products'>Number of Products {props.productsNumber}</div>
-        <div className='filter-by-size'>
-          <span>Filter</span>
-          <select className='filter-select' onChange={props.handelFilterBySize} value={props.size}>
-            <option value="ALL">ALL</option>
-            <option value="XS">XS</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-            <option value="L">L</option>
-            <option value="XL">XL</option>
-            <option value="XXL">XXL</option>
-          </select>
-        </div>
-        <div className='filter-by-size'>
-          <span>Order</span>
-          <select className='filter-select' value={props.sort} onChange={props.handelFilterByOrder}>
-            <option value="Latest">Latest</option>
-            <option value="Lowest">Lower</option>
-            <option value="Highest">Highest</option>
-
-          </select>
-        </div>
+      {props.filterProducts && <div className="Filter-wrapper">
+      <h2 className="Filter-title">Filter</h2>
+      <div className='num-of-products'>Number of Products {props.filterProducts.length}</div>
+      <div className='filter-by-size'>
+        <span>Filter</span>
+        <select className='filter-select' onChange={(e) => props.filteredSize(props.filterProducts, e.target.value)} value={props.size}>
+          <option value="ALL">ALL</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
       </div>
+      <div className='filter-by-size'>
+        <span>Order</span>
+        <select className='filter-select' value={props.sort} onChange={(e) => props.filteredSort(props.products, e.target.value)}>
+          <option value="Latest">Latest</option>
+          <option value="Lowest">Lower</option>
+          <option value="Highest">Highest</option>
+
+        </select>
+      </div>
+    </div>}
     </Filp>
   )
 }
 
-export default Filter;
+export default connect((state) => {
+  return {
+    sort: state.products.sort,
+    size: state.products.size,
+    products: state.products.products,
+    filterProducts: state.products.filterProducts,
+  }
+}, {
+  filteredSize, filteredSort
+})(Filter);
--- client/src/store/actions/types.js
+++ client/src/store/actions/types.js
@@ -1,2 +1,7 @@
+
+// Products
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS"
 
+//Fillter
+export const FILTER_SIZE = "FILTER_SIZE"
+export const FILTER_SORT = "FILTER_SORT"
